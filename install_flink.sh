6276ae76af04cb72b5642bf82a29e2ab58c75a1b
TjWallas <tjwallas@cryptolab.net>
2016-06-29 15:39:23 +0200

    Bumps down flink version to 2.10
    In order to be compatible with the gelly-streaming library

--- install_flink.sh
+++ install_flink.sh
@@ -1,3 +1,3 @@
-sudo wget -nc -O /opt/flink-1.0.3-bin-hadoop27-scala_2.11.tgz http://apache.trisect.eu/flink/flink-1.0.3/flink-1.0.3-bin-hadoop27-scala_2.11.tgz &&
-sudo tar -xzvf /opt/flink-1.0.3-bin-hadoop27-scala_2.11.tgz -C /opt/ &&
+sudo wget -nc -O /opt/flink-1.0.3-bin-hadoop27-scala_2.10.tgz http://ftp.halifax.rwth-aachen.de/apache/flink/flink-1.0.3/flink-1.0.3-bin-hadoop27-scala_2.10.tgz &&
+sudo tar -xzvf /opt/flink-1.0.3-bin-hadoop27-scala_2.10.tgz -C /opt/ &&
 grep -q 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' /home/vagrant/.bashrc || echo 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' >> /home/vagrant/.bashrc
