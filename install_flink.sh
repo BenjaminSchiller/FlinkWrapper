a1220dba36e0e034b9fbef66e19bace066083b87
TjWallas <tjwallas@cryptolab.net>
2016-06-29 16:09:41 +0200

    Changes permissions to /opt/flink to a=rwx
    This is important because flink will try to place its log and temporary files in that location

--- install_flink.sh
+++ install_flink.sh
@@ -1,3 +1,4 @@
 sudo wget -nc -O /opt/flink-1.0.3-bin-hadoop27-scala_2.10.tgz http://ftp.halifax.rwth-aachen.de/apache/flink/flink-1.0.3/flink-1.0.3-bin-hadoop27-scala_2.10.tgz &&
 sudo tar -xzvf /opt/flink-1.0.3-bin-hadoop27-scala_2.10.tgz -C /opt/ &&
-grep -q 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' /home/vagrant/.bashrc || echo 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' >> /home/vagrant/.bashrc
+grep -q 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' /home/vagrant/.bashrc || echo 'export PATH="/opt/flink-1.0.3/bin/:$PATH"' >> /home/vagrant/.bashrc &&
+sudo chmod -R a=rwx /opt/flink-1.0.3
